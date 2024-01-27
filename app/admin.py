c3e0eeaef39745015d90f50be88bc3645325cd67
Mikhail Moskovkin <anstane228@gmail.com>
2024-01-27 13:49:17 +0300

    Update of DB

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Product,
+    Recipe,
+    RecipeProduct
+)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'times_used'
+    )
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'display_products'
+    )
+
+    def display_products(self, obj):
+         return ', '.join([product.name for product in obj.product.all()])
+
+
+@admin.register(RecipeProduct)
+class RecipeProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'recipe',
+        'product',
+        'weight_in_grams'
+    )
--- app/models.py
+++ app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    times_used = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=150)
+    product = models.ManyToManyField(Product, through='RecipeProduct')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
+
+class RecipeProduct(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    weight_in_grams = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.product.name} - {self.weight_in_grams}г'
+    
+    def save(self, *args, **kwargs):
+        self.product.times_used += 1
+        self.product.save()
+        super(RecipeProduct, self).save(*args, **kwargs) 
+
+    class Meta:
+        verbose_name = 'Рецепт - продукт'
+        verbose_name_plural = 'Рецепты - продукты'
+        constraints = (
+            models.UniqueConstraint(
+                fields=['recipe', 'product'],
+                name='unique_recipe_product'
+            ),
+        )
